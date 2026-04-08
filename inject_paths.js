a585ce4ead557dfb69561cc1cf916d9f6874e82b
aadams-bga <aadams@bettergov.org>
2026-04-08 17:06:23 -0500

    cleaned up map, using dummy text. removed old files.

--- inject_paths.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const template = fs.readFileSync('illinois-map-animation.html', 'utf8');
-const paths = fs.readFileSync('county_paths.txt', 'utf8');
-
-const finalHtml = template.replace('<g id="county-group"></g>', `<g id="county-group">${paths}</g>`);
-
-fs.writeFileSync('illinois-map-animation.html', finalHtml);
-console.log('Successfully injected county paths into the prototype.');
